5e31870a6905a749b4d6d55f01b265798d6d8933
DESKTOP-RJ6VDHV\Nicholas <nicholas_tankard@hotmail.com>
2017-11-30 20:50:36 +1000

    Repair

--- src/com/ntankard/DynamicGUI/Unused/BoundEvent_JButton.java
+++ src/com/ntankard/DynamicGUI/Unused/BoundEvent_JButton.java
@@ -1,22 +1,22 @@
-package com.ntankard.DynamicGUI.Unused;
-
-import com.ntankard.DynamicGUI.Components.EditablePanel.Primitives.DataBinding.Bindable;
-import com.ntankard.DynamicGUI.Unused.Event.Event;
-
-/**
- * Created by Nicholas on 30/06/2016.
- */
-public class BoundEvent_JButton extends Bound_JButton {
-
-    private Bindable<Event> data;
-
-    public BoundEvent_JButton(String name, Bindable<Event> data) {
-        super(name);
-        this.data = data;
-        addActionListener(e -> onClick());
-    }
-
-    private void onClick() {
-        // BoundStructure_Dialog.openPanel(data.get().getPanel());
-    }
-}
+//package com.ntankard.DynamicGUI.Unused;
+//
+//import com.ntankard.DynamicGUI.Components.EditablePanel.Primitives.DataBinding.Bindable;
+//import com.ntankard.DynamicGUI.Unused.Event.Event;
+//
+///**
+// * Created by Nicholas on 30/06/2016.
+// */
+//public class BoundEvent_JButton extends Bound_JButton {
+//
+//    private Bindable<Event> data;
+//
+//    public BoundEvent_JButton(String name, Bindable<Event> data) {
+//        super(name);
+//        this.data = data;
+//        addActionListener(e -> onClick());
+//    }
+//
+//    private void onClick() {
+//        // BoundStructure_Dialog.openPanel(data.get().getPanel());
+//    }
+//}
--- src/com/ntankard/DynamicGUI/Unused/Event/Event.java
+++ src/com/ntankard/DynamicGUI/Unused/Event/Event.java
@@ -1,28 +1,16 @@
-package com.ntankard.DynamicGUI.Unused.Event;
-
-
-import java.io.Serializable;
-
-/**
- * Created by Nicholas on 5/06/2016.
- */
-public abstract class Event implements Serializable, BoundStructure_Generator {
-
-    private static final long serialVersionUID = 3306006568441153813L;
-
-    public enum State {UNINITIALIZED, USER_INPUT_NEEDED, USER_ACTION_NEEDED, READY}
-
-    private State state;
-
-    public abstract void execute();
-
-    public BoundStructure_Generator clone() {
-        return null;
-    }
-
-    // public BoundStructure_JPanel getEditPanel(){
-    //    return null;
-    //  }
-
-
-}
+//package com.ntankard.DynamicGUI.Unused.Event;
+//
+//
+//import java.io.Serializable;
+//
+///**
+// * Created by Nicholas on 5/06/2016.
+// */
+//public abstract class Event implements Serializable, BoundStructure_Generator {
+//
+//    private static final long serialVersionUID = 3306006568441153813L;
+//
+//    public enum State {UNINITIALIZED, USER_INPUT_NEEDED, USER_ACTION_NEEDED, READY}
+//
+//    private State state;
+//}
